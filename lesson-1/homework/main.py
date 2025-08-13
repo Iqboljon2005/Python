2d73f78229e02f5853d8a4af5e0ae3e853dfff05
Iqboljon2005 <125290960+Iqboljon2005@users.noreply.github.com>
2025-08-13 14:26:53 +0500

    Create main.py

---
+++ lesson-1/homework/main.py
@@ -0,0 +1,50 @@
+# 1
+
+side = float(input("Enter the side length of the square: "))
+
+perimeter = 4 * side
+area = side ** 2
+
+print(f"Perimeter of the square: {perimeter}")
+print(f"Area of the square: {area}")
+
+
+
+
+# 2
+
+import math
+
+diameter = float(input("Enter the diameter of the circle: "))
+
+calculation = math.pi * diameter
+
+print(f"Circumference of the circle: {calculation}")
+
+
+
+# 3 
+
+a = float(input("Enter first number (a): "))
+b = float(input("Enter second number (b): "))
+
+mean = (a + b) / 2
+
+print(f"Mean of {a} and {b} is: {mean}")
+
+
+
+# 4
+
+a = float(input("Enter first number (a): "))
+b = float(input("Enter second number (b): "))
+
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print(f"Sum: {sum_ab}")
+print(f"Product: {product_ab}")
+print(f"Square of a: {square_a}")
+print(f"Square of b: {square_b}")
